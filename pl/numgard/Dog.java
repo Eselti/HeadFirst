ad182a082723b299399b4ee0c934bf7b0575669f
BartC <bartlomiej.ciszewski@gmail.com>
2017-09-26 14:29:55 +0200

    dodanepliki

---
+++ pl/numgard/Dog.java
@@ -0,0 +1,15 @@
+package pl.numgard;
+
+ class Dog {
+	
+	 int size;
+	 String rasa;
+	 String imie;
+	 
+	 void bark(String x){
+		 
+		 System.out.println("woof woof" + x);
+		 
+	 }
+
+}
--- pl/numgard/Test.java
+++ pl/numgard/Test.java
@@ -5,6 +5,11 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("bla");
+		
+		
+		Dog d = new Dog();
+		d.size = 40;
+		d.bark(" bark!");
 
 	}
 
